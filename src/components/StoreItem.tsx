0c3b40c6fe6542a6d9e6872eef2b78fd37ddac4f
yasindu <yazindu@gmail.com>
2023-08-18 22:36:46 +0530

    feat: at 27:03. fixed lost hyphen in flex-column

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,37 @@
+import {Button, Card} from "react-bootstrap";
+import {formatCurrency} from "../utilities/formatCurrency.ts";
+
+
+type StoreItemProps = {
+    id: number
+    name: string
+    price: number
+    imgUrl: string
+}
+
+export const StoreItem = ({id, imgUrl, price, name}: StoreItemProps) => {
+    const quantity = 0
+    return (
+            <Card className="h-100">
+                <Card.Img
+                    variant="top"
+                    src={imgUrl}
+                    height="200px"
+                    style={{objectFit: "cover"}}
+                />
+                <Card.Body className="d-flex flex-column">
+                    <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                        <span className="fs-2">{name}</span>
+                        <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+                    </Card.Title>
+                    <div className="mt-auto">
+                        {quantity === 0
+                            ? (<Button className="w-100">+ Add To Cart</Button>)
+                            : null
+                        }
+                    </div>
+                </Card.Body>
+
+            </Card>
+    )
+}
