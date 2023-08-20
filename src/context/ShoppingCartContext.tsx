a2a7cfb1a554132cf386d60b8dba88e1a41d5c1c
yasindu <yazindu@gmail.com>
2023-08-20 07:13:24 +0530

    feat: at 39:40. created getItemQuantity, increaseCartQuantity, decreaseCartQuantity and removeFromCart

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,18 +1,70 @@
-import {createContext, ReactNode, useContext} from "react";
+import {createContext, ReactNode, useContext, useState} from "react";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
 }
 
-const ShoppingCartContext = createContext({})
+type ShoppingCartContext = {
+    getItemQuantity: (id: number) => number
+    increaseCartQuantity: (id: number) => void
+    decreaseCartQuantity: (id: number) => void
+    removeFromCart: (id: number) => void
+}
+
+type CartItem = {
+    id: number
+    quantity: number
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
 export const useShoppingCart = () => { //Custom hook
     return useContext(ShoppingCartContext)
 }
 
 export function ShoppingCartProvider({children}: ShoppingCartProviderProps) { // provide portion : provider gives all the values and houses the code for rendering the shopping cart when we click the shopping cart button
+    const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+    function getItemQuantity(id: number) {
+        return cartItems.find(item => item.id === id)?.quantity || 0
+    }
+
+    function increaseCartQuantity(id: number) {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id) == null) {
+                return [...currItems, {id: id, quantity: 1}]
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return {...item, quantity: ++item.quantity}
+                    } else return item
+                })
+            }
+        })
+    }
+    function decreaseCartQuantity(id: number) {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id)?.quantity === 1) {
+                return currItems.filter(item => item.id !== id)
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return {...item, quantity: --item.quantity}
+                    } else return item
+                })
+            }
+        })
+    }
+
+    function removeFromCart(id: number) {
+        setCartItems(currItems => {
+            return currItems.filter(item => item.id !== id)
+        })
+    }
+
+
     return (
-        <ShoppingCartContext.Provider value={{}}>
+        <ShoppingCartContext.Provider value={{getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
             {children}
         </ShoppingCartContext.Provider>
     )
