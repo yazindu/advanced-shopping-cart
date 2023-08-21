b48cc790b5d8de4d3a1d760a16e830f394e7789b
yasindu <yazindu@gmail.com>
2023-08-21 07:11:37 +0530

    feat: Corrected SVG attributes to avoid Invalid DOM property warning in browser

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -28,7 +28,7 @@ export const NavBar = () => {
                         <circle cx="184" cy="216" r="14"/>
                         <path
                             d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
-                            fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="12"/>
+                            fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/>
                     </svg>
                     <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
                          style={{
